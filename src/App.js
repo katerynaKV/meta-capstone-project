233a436a5aa2a8514ccff47e6203de36f5d12e3e
katerynaKV <bludova.ekaterina26@gmail.com>
2026-02-18 20:21:07 +0100

    Added confirmation page and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Route, Routes } from "react-router";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { ReservationPage } from "./pages/ReservationPage/ReservationPage";
+import { ConfirmationPage } from "./pages/ConfirmationPage/ConfirmationPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reservation" element={<ReservationPage />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ConfirmationPage/ConfirmationPage.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router";
+import { Button } from "../../components/Button/Button";
+import { Page } from "../../components/Page/Page";
+
+import "./ConfirmationPage.css";
+
+export function ConfirmationPage() {
+  return (
+    <Page>
+      <div className="table-is-reserved">
+        <h2>Your table is reserved!</h2>
+      </div>
+
+      <div className="container  confirmation-content">
+        <div className="confirmation-wrapper">
+          <div className="confirmation-details">
+            <h3>Reservation details</h3>
+            <div className="detail-container">
+              <b>Date:</b>
+              <span>23.02.2026</span>
+            </div>
+
+            <div className="detail-container">
+              <b>Time:</b>
+              <span>18:00</span>
+            </div>
+
+            <div className="detail-container">
+              <b>Number of guests:</b>
+              <span>3</span>
+            </div>
+            <div className="detail-container">
+              <b>Name:</b>
+              <span>John Doe</span>
+            </div>
+            <div className="detail-container">
+              <b>Phone number:</b>
+              <span>556622448</span>
+            </div>
+            <div className="detail-container">
+              <b>Email:</b>
+              <span>shos.tam@example.com</span>
+            </div>
+          </div>
+
+          <div className="confirmation-img">
+            <img src="/restaurant-chef.jpg" />
+          </div>
+        </div>
+        <Link to="/" className="confirmation-done">
+          <Button>Back to home</Button>
+        </Link>
+      </div>
+    </Page>
+  );
+}
